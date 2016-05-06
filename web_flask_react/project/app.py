0397f90c6293fd883f1db2315c301bd03b09f323
Jeff <jeffchen91430@gmail.com>
2016-05-05 23:20:31 -0400

    Added React code for click to show/hide divs

--- web_flask_react/project/app.py
+++ web_flask_react/project/app.py
@@ -8,6 +8,11 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/hello')
+def hello():
+    return render_template('hello.html')
+
+
 @app.route('/query')
 def process_query():
 	query = request.args.get('query')
--- web_flask_react/project/static/scripts/js/main.js
+++ web_flask_react/project/static/scripts/js/main.js
@@ -1,63 +1,171 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-/** @jsx React.DOM */
-
-var DynamicSearch = React.createClass({displayName: "DynamicSearch",
-
-  // sets initial state
-  getInitialState: function(){
-    return { searchString: '' };
+/*var FreshmanSelected = React.createClass({
+  getInitialState: function() {
+    return {showMe: false};
   },
-
-  // sets state, triggers render method
-  handleChange: function(event){
-    // grab value form input box
-    this.setState({searchString:event.target.value});
-    console.log("scope updated!")
+  onClick: function(event) {
+    this.setState({showMe: !this.state.showMe});
+    console.log("CLICK");
   },
-
   render: function() {
-
-    var countries = this.props.items;
-    var searchString = this.state.searchString.trim().toLowerCase();
-
-    // filter countries list by value from input box
-    if(searchString.length > 0){
-      countries = countries.filter(function(country){
-        return country.name.toLowerCase().match( searchString );
-      });
+    if (this.state.showMe) {
+      document.getElementById('yr1L').setAttribute("style", "");
+      document.getElementById('yr1R').setAttribute("style", "");
+      return <p onClick={this.onClick}>Visible</p>;
     }
-
-    return (
-      React.createElement("div", null, 
-        React.createElement("input", {type: "text", value: this.state.searchString, onChange: this.handleChange, placeholder: "Search!"}), 
-        React.createElement("ul", null, 
-           countries.map(function(country){ return React.createElement("li", null, country.name, " ") }) 
-        )
-      )
-    )
+    else {
+      document.getElementById('yr1L').setAttribute("style", "display:none;");
+      document.getElementById('yr1R').setAttribute("style", "display:none;");
+      return <p onClick={this.onClick}>Not visible</p>;
+    }
   }
-
 });
 
-// list of countries, defined with JavaScript object literals
-var countries = [
-  {"name": "Sweden"}, {"name": "China"}, {"name": "Peru"}, {"name": "Czech Republic"},
-  {"name": "Bolivia"}, {"name": "Latvia"}, {"name": "Samoa"}, {"name": "Armenia"},
-  {"name": "Greenland"}, {"name": "Cuba"}, {"name": "Western Sahara"}, {"name": "Ethiopia"},
-  {"name": "Malaysia"}, {"name": "Argentina"}, {"name": "Uganda"}, {"name": "Chile"},
-  {"name": "Aruba"}, {"name": "Japan"}, {"name": "Trinidad and Tobago"}, {"name": "Italy"},
-  {"name": "Cambodia"}, {"name": "Iceland"}, {"name": "Dominican Republic"}, {"name": "Turkey"},
-  {"name": "Spain"}, {"name": "Poland"}, {"name": "Haiti"}
-];
+var SophomoreSelected = React.createClass({
+  getInitialState: function() {
+    return {showMe: false};
+  },
+  onClick: function(event) {
+    this.setState({showMe: !this.state.showMe});
+    FreshmanSelected.setState({showMe: false});
+    console.log("CLICK");
+  },
+  render: function() {
+    if (this.state.showMe) {
+      document.getElementById('yr2L').setAttribute("style", "");
+      document.getElementById('yr2R').setAttribute("style", "");
+      return <p onClick={this.onClick}>Visible</p>;
+    }
+    else {
+      document.getElementById('yr2L').setAttribute("style", "display:none;");
+      document.getElementById('yr2R').setAttribute("style", "display:none;");
+      return <p onClick={this.onClick}>Not visible</p>;
+    }
+  }
+});
 
-React.render(
-  React.createElement(DynamicSearch, {items:  countries }),
-  document.getElementById('main')
-);
+ReactDOM.render(<FreshmanSelected />, document.getElementById('freshman'));
+ReactDOM.render(<SophomoreSelected />, document.getElementById('sophomore'));
+*/
+
+var YearSelect = React.createClass({displayName: "YearSelect",
+  getInitialState: function() {
+    return {year: null};
+  },
+  freshmanSelect: function(event) {
+    this.setState({year: "frosh"});
+    console.log("FROSH");
+  },
+  sophomoreSelect: function(event) {
+    this.setState({year: "soph"});
+    console.log("SOPH");
+  },
+  juniorSelect: function(event) {
+    this.setState({year: "junr"});
+    console.log("JUNR");
+  },
+  render: function() {
+    if (this.state.year == "frosh") {
+      document.getElementById('yr1L').setAttribute("style", "");
+      document.getElementById('yr1R').setAttribute("style", "");
+      document.getElementById('yr2L').setAttribute("style", "display:none;");
+      document.getElementById('yr2R').setAttribute("style", "display:none;");
+      document.getElementById('yr3L').setAttribute("style", "display:none;");
+      document.getElementById('yr3R').setAttribute("style", "display:none;");
+    }
+    if (this.state.year == "soph") {
+      document.getElementById('yr1L').setAttribute("style", "");
+      document.getElementById('yr1R').setAttribute("style", "");
+      document.getElementById('yr2L').setAttribute("style", "");
+      document.getElementById('yr2R').setAttribute("style", "");
+      document.getElementById('yr3L').setAttribute("style", "display:none;");
+      document.getElementById('yr3R').setAttribute("style", "display:none;");
+    }
+    if (this.state.year == "junr") {
+      document.getElementById('yr1L').setAttribute("style", "");
+      document.getElementById('yr1R').setAttribute("style", "");
+      document.getElementById('yr2L').setAttribute("style", "");
+      document.getElementById('yr2R').setAttribute("style", "");
+      document.getElementById('yr3L').setAttribute("style", "");
+      document.getElementById('yr3R').setAttribute("style", "");
+    }
+    return (
+      React.createElement("div", null, 
+      "What year are you?", React.createElement("br", null), 
+        React.createElement("div", {id: "freshman", onClick: this.freshmanSelect}, 
+          "2019"
+        ), 
+        React.createElement("div", {id: "sophomore", onClick: this.sophomoreSelect}, 
+          "2018"
+        ), 
+        React.createElement("div", {id: "junior", onClick: this.juniorSelect}, 
+          "2017"
+        )
+      )
+    );
+  }
+});
+ReactDOM.render(React.createElement(YearSelect, null), document.getElementById('yearInput'));
+
+
+/*var ShowCourseHistInput = React.createClass({
+  getInitialState: function() {
+    return {clicked: false};
+  },
+  handleClick: function(event) {
+    this.setState({clicked: !this.state.clicked});
+    console.log("CLICK");
+  },
+  render: function() {
+    var text = this.state.clicked ? 'like' : 'haven\'t liked';
+    return (
+      <div onClick={this.handleClick}>
+        You {text} this. Click to toggle.
+      </div>
+      <div onClick={this.handleClick}>
+        {this.state.clicked ? <Year /> : null}
+      </div>
+    );
+  }
+});
+
+var Year = React.createClass({
+  render: function() {
+    return (
+      <div id="yr1L">TEXT</div>
+    );
+  }
+});
+
+var Search = React.createClass({
+    getInitialState: function() {
+        return { showResults: false };
+    },
+    onClick: function() {
+        this.setState({ showResults: !this.state.showResults});
+    },
+    render: function() {
+        return (
+            <div>
+                <input type="submit" value="Search" onClick={this.onClick} />
+                { this.state.showResults ? <Results /> : null }
+            </div>
+        );
+    }
+});
+
+var Results = React.createClass({
+    render: function() {
+        return (
+            <div id="results" className="search-results">
+                Some Results
+            </div>
+        );
+    }
+});
+
+ReactDOM.render(<Search />, document.getElementById('freshman'));*/
+
 
 
 },{}]},{},[1])
-
-var showInputForms = React.createClass({
-
-});
