158a630fae2dfe3817136c8405852f998688b8e3
Christopher Hay <chay@princeton.edu>
2016-05-07 03:00:45 -0400

    Added term info to recommendation results

--- web_flask_react/project/course.py
+++ web_flask_react/project/course.py
@@ -5,9 +5,20 @@ class Course(object):
 	'Papers; Reports; Problem Sets; Examinations':1, 
 	'Readings':2, 'Classes':3, 'Overall Quality of the Course':4, 
 	'Feedback for other students:':5}
+
+	term_ids = {'1142': "Fall '13", '1144': "Spring '14",
+	            '1152': "Fall '14", '1154': "Spring '15", 
+	            '1162': "Fall '15", '1164': "Spring '16", 
+	            '1172': "Fall '16"}
 	def __init__(self, term_info_dict):
 		self.term_info_dict = term_info_dict
-		self.default_term = sorted(term_info_dict.keys())[-1]
+		for term_id in sorted(term_info_dict.keys(), reverse=True):
+			if len(self.term_info_dict[term_id]['EVAL']) > 0:
+				self.default_term = term_id
+				break
+
+	def get_default_term_text(self):
+		return self.term_ids[self.default_term]
 
 	def get_title(self, term_id=None):
 		if term_id is None:
