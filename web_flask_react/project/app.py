cf4488020029eea9a0323ba44e80f38c309251d5
Jeff <jeffchen91430@gmail.com>
2016-05-08 16:33:17 -0400

    Accidentally touched app.py

--- web_flask_react/project/app.py
+++ web_flask_react/project/app.py
@@ -10,7 +10,7 @@ from search import Searcher
 app = Flask(__name__)
 
 recommender = Recommender(os.path.join(os.getcwd(), 'project/static/recommender_necessities.pickle'))
-#searcher = Searcher(os.path.join(os.getcwd(), 'project/static/search_necessities.pickle'))
+searcher = Searcher(os.path.join(os.getcwd(), 'project/static/search_necessities.pickle'))
 course_renderer = CourseRenderer(os.path.join(os.getcwd(),'project/static/course_info_necessities.pickle'))
 
 max_results = 20
