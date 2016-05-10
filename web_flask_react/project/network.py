59e0f2164f8c5766d67a441deb5a5eae1cf18c41
Christopher Hay <chay@princeton.edu>
2016-05-10 01:27:44 -0400

    Added network stats and graphs

--- web_flask_react/project/network.py
+++ web_flask_react/project/network.py
@@ -13,7 +13,7 @@ G.add_nodes_from(course_id_list)
 for course_id, mentions_dict in course_association_dictionary.items():
 	for mention, num in mentions_dict.items():
 		if not course_id == mention:
-			G.add_edge(course_id, mention, num)	
+			G.add_edge(course_id, mention, weight=num)	
 
 for node in G.nodes():
 	if G.degree(node) == 0:
@@ -24,5 +24,49 @@ for key, val in class_number_lookup_dict.items():
 	if key in G.node:
 		labels_dict[key] = ' '.join(val)
 
-nx.draw_networkx(G, node_size=50, font_size=10, labels=labels_dict)
-plt.show()
+# nx.draw_networkx(G, node_size=50, font_size=10, labels=labels_dict)
+# plt.show()
+
+pagerank = nx.pagerank(G)
+print 'PAGERANK'
+for val, key in sorted([(val, key) for key, val in pagerank.items()], reverse=True)[:10]:
+	print labels_dict[key], ':', val
+
+hubs, authorities = nx.hits(G)
+print 'HUBS'
+for val, key in sorted([(val, key) for key, val in hubs.items()], reverse=True)[:10]:
+	print labels_dict[key], ':', val
+
+print 'AUTHORITIES'
+for val, key in sorted([(val, key) for key, val in authorities.items()], reverse=True)[:10]:
+	print labels_dict[key], ':', val
+
+mean = lambda x: 1.*sum(x) / len(x)
+print 'Mean degree', mean(nx.degree(G).values())
+print 'Max degree', max(nx.degree(G).values())
+print 'TOP DEGREE NODES'
+for val, key in sorted([(val, key) for key, val in nx.degree(G).items()], reverse=True)[:10]:
+	print labels_dict[key], ':', val
+
+for size, clique_list in sorted([(len(clique_list), clique_list) for clique_list in nx.find_cliques(G)], reverse=True)[:10]:
+	print [labels_dict[k] for k in clique_list], ':', size
+
+
+richclub = nx.rich_club_coefficient(G)
+
+
+subgraphs = [g for size, g in sorted([(g.number_of_nodes(), g) for g in nx.connected_component_subgraphs(G)], reverse=True)]
+for i, g in enumerate(subgraphs[:10]):
+	print 'For subgraph', i
+	print 'Number of nodes', g.number_of_nodes()
+	print 'Number of edges', g.number_of_edges()
+	print 'Center:', [labels_dict[k] for k in nx.center(g)]	
+	print 'Radius:', nx.radius(g)
+	# print 'Avg. Node Connectivity', nx.average_node_connectivity(g)
+	print 'Avg. Shortest Path Length', nx.average_shortest_path_length(g)
+	cur_labels_dict = defaultdict(str)
+	for key, val in class_number_lookup_dict.items():
+		if key in g.node:
+			cur_labels_dict[key] = val[0]
+	nx.draw_networkx(g, linewidths=0, node_color='#33ccff', edge_color='#cccccc', labels=cur_labels_dict)
+	plt.show()
